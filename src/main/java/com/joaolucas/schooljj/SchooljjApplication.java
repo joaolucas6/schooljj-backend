25b94613216380c09ab67aa0cfcb9599c9bcfcb6
joaolucas6 <joaolucaspereiraramos06@gmail.com>
2023-09-07 09:09:16 -0300

    feat: first commit

---
+++ src/main/java/com/joaolucas/schooljj/SchooljjApplication.java
@@ -0,0 +1,13 @@
+package com.joaolucas.schooljj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchooljjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchooljjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joaolucas/schooljj/SchooljjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaolucas.schooljj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchooljjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
