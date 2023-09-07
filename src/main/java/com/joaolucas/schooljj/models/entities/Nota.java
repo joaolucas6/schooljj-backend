4963ff867c440a02c6e7227ee307e16df2cb876f
joaolucas6 <joaolucaspereiraramos06@gmail.com>
2023-09-07 10:24:23 -0300

    feat: criando entidade Nota

--- src/main/java/com/joaolucas/schooljj/models/entities/Nota.java
+++ src/main/java/com/joaolucas/schooljj/models/entities/Nota.java
@@ -1,4 +1,118 @@
 package com.joaolucas.schooljj.models.entities;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_nota")
 public class Nota {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nota")
+    private Double nota;
+
+    @ManyToOne
+    @JoinColumn(name = "tarefa_id")
+    private Tarefa tarefa;
+
+    @ManyToOne
+    @JoinColumn(name = "professor_id")
+    private Professor professor;
+
+    @Column(name = "observacoes")
+    private String observacoes;
+
+    @Column(name = "data_criacao")
+    private LocalDateTime dataCriacao;
+
+    public Nota(){
+
+    }
+
+    public Nota(Long id, Double nota, Tarefa tarefa, Professor professor, String observacoes, LocalDateTime dataCriacao) {
+        this.id = id;
+        this.nota = nota;
+        this.tarefa = tarefa;
+        this.professor = professor;
+        this.observacoes = observacoes;
+        this.dataCriacao = dataCriacao;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Double getNota() {
+        return nota;
+    }
+
+    public void setNota(Double nota) {
+        this.nota = nota;
+    }
+
+    public Tarefa getTarefa() {
+        return tarefa;
+    }
+
+    public void setTarefa(Tarefa tarefa) {
+        this.tarefa = tarefa;
+    }
+
+    public Professor getProfessor() {
+        return professor;
+    }
+
+    public void setProfessor(Professor professor) {
+        this.professor = professor;
+    }
+
+    public String getObservacoes() {
+        return observacoes;
+    }
+
+    public void setObservacoes(String observacoes) {
+        this.observacoes = observacoes;
+    }
+
+    public LocalDateTime getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(LocalDateTime dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        Nota nota = (Nota) object;
+        return Objects.equals(id, nota.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Nota{" +
+                "id=" + id +
+                ", nota=" + nota +
+                ", tarefa=" + tarefa +
+                ", professor=" + professor +
+                ", observacoes='" + observacoes + '\'' +
+                ", dataCriacao=" + dataCriacao +
+                '}';
+    }
 }
