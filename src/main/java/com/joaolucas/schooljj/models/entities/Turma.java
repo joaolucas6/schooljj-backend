0d1298cfe7af288f233d709b968668512603df84
joaolucas6 <joaolucaspereiraramos06@gmail.com>
2023-09-07 09:53:47 -0300

    feat: criando entidade Turma

--- src/main/java/com/joaolucas/schooljj/models/entities/Turma.java
+++ src/main/java/com/joaolucas/schooljj/models/entities/Turma.java
@@ -1,4 +1,94 @@
 package com.joaolucas.schooljj.models.entities;
 
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_turma")
 public class Turma {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @ManyToMany
+    @JoinTable(
+            name = "turma_professores",
+            joinColumns = @JoinColumn(name = "turma_id"),
+            inverseJoinColumns = @JoinColumn(name = "professor_id")
+    )
+    private List<Professor> professores = new ArrayList<>();
+
+    @OneToMany(mappedBy = "turma")
+    private List<Aluno> alunos = new ArrayList<>();
+
+    public Turma(){
+
+    }
+
+    public Turma(Long id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Professor> getProfessores() {
+        return professores;
+    }
+
+    public void setProfessores(List<Professor> professores) {
+        this.professores = professores;
+    }
+
+    public List<Aluno> getAlunos() {
+        return alunos;
+    }
+
+    public void setAlunos(List<Aluno> alunos) {
+        this.alunos = alunos;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        Turma turma = (Turma) object;
+        return Objects.equals(id, turma.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Turma{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", professores=" + professores +
+                ", alunos=" + alunos +
+                '}';
+    }
 }
