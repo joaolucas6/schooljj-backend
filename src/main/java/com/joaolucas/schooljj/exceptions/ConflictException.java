e24a48b9f6a227ce826290edc551ab618a987bba
joaolucas6 <joaolucaspereiraramos06@gmail.com>
2023-09-07 16:52:29 -0300

    feat: criando ConflictException

---
+++ src/main/java/com/joaolucas/schooljj/exceptions/ConflictException.java
@@ -0,0 +1,7 @@
+package com.joaolucas.schooljj.exceptions;
+
+public class ConflictException extends RuntimeException{
+    public ConflictException(String message){
+        super(message);
+    }
+}
