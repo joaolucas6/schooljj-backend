4d492d1838125933e1b91b7169f33f96fd776859
joaolucas6 <joaolucaspereiraramos06@gmail.com>
2023-09-08 11:06:26 -0300

    feat: criando DisciplinaService

---
+++ src/main/java/com/joaolucas/schooljj/services/DisciplinaService.java
@@ -0,0 +1,52 @@
+package com.joaolucas.schooljj.services;
+
+import com.joaolucas.schooljj.exceptions.BadRequestException;
+import com.joaolucas.schooljj.exceptions.ResourceNotFoundException;
+import com.joaolucas.schooljj.models.dto.DisciplinaDTO;
+import com.joaolucas.schooljj.models.entities.Disciplina;
+import com.joaolucas.schooljj.repositories.DisciplinaRepository;
+import com.joaolucas.schooljj.utils.DataValidation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class DisciplinaService {
+
+    private final DisciplinaRepository disciplinaRepository;
+
+    public List<DisciplinaDTO> findAll(){
+        return disciplinaRepository.findAll().stream().map(DisciplinaDTO::new).toList();
+    }
+
+    public DisciplinaDTO findById(Long id){
+        return new DisciplinaDTO(disciplinaRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Disciplina não foi encontrada com ID: " + id)));
+    }
+
+    public DisciplinaDTO create(DisciplinaDTO disciplinaDTO){
+        if(!DataValidation.isDisciplinaDataValid(disciplinaDTO)) throw new BadRequestException("Os dados da disciplina são inválidos.");
+        Disciplina disciplina = new Disciplina();
+        disciplina.setNome(disciplinaDTO.getNome());
+        disciplina.setDescricao(disciplinaDTO.getDescricao());
+        return new DisciplinaDTO(disciplinaRepository.save(disciplina));
+    }
+
+    public DisciplinaDTO update(Long id, DisciplinaDTO disciplinaDTO){
+        if(!DataValidation.isDisciplinaDataValid(disciplinaDTO)) throw new BadRequestException("Os dados da disciplina são inválidos.");
+        Disciplina disciplina = disciplinaRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Disciplina não foi encontrada com ID: " + id));
+
+        if(disciplinaDTO.getNome() != null) disciplina.setNome(disciplinaDTO.getNome());
+        if(disciplinaDTO.getDescricao() != null) disciplina.setDescricao(disciplinaDTO.getDescricao());
+
+        return new DisciplinaDTO(disciplinaRepository.save(disciplina));
+    }
+
+    public void delete(Long id){
+        Disciplina disciplina = disciplinaRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Disciplina não foi encontrada com ID: " + id));
+        disciplinaRepository.delete(disciplina);
+    }
+
+
+}
